ef0d51393ace8b884fb0e2b8a0f077d7ccb32658
yutaro okuda <yutaro.okuda.76@gmail.com>
2023-11-01 08:38:39 +0800

    display tag in view

--- config/Seeds/TagsSeed.php
+++ config/Seeds/TagsSeed.php
@@ -44,14 +44,14 @@ class TagsSeed extends AbstractSeed
                 'post_id' => 1,
                 'tag_id' => 1,
             ],[
-                'post_id' => 1,
-                'tag_id' => 2,
-            ],[
-                'post_id' => 1,
-                'tag_id' => 2,
-            ],[
                 'post_id' => 2,
                 'tag_id' => 2,
+            ],[
+                'post_id' => 3,
+                'tag_id' => 3,
+            ],[
+                'post_id' => 4,
+                'tag_id' => 1,
             ]
         ];
 
